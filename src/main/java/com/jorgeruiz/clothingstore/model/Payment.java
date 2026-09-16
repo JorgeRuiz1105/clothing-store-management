ecb33dbb37386386589699e1752b0ac2d9eb8600
Jorge Ruiz <jorge_ruiz82212@elpoli.edu.co>
2026-09-16 18:47:54 -0500

    feat(payment): add Payment entity

---
+++ src/main/java/com/jorgeruiz/clothingstore/model/Payment.java
@@ -0,0 +1,48 @@
+package com.jorgeruiz.clothingstore.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private int id;
+    private BigDecimal amount;
+    private LocalDateTime date;
+    private Debt debt;
+
+    public Payment(int id, BigDecimal amount, Debt debt) {
+        this.id = id;
+        this.amount = validateAmount(amount);
+        this.date = LocalDateTime.now();
+        this.debt = validateDebt(debt);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public Debt getDebt() {
+        return debt;
+    }
+
+    private BigDecimal validateAmount(BigDecimal amount){
+        if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("Ha introducido un valor de abono invalido!");
+        }
+        return amount;
+    }
+
+    private Debt validateDebt(Debt debt){
+        if(debt == null){
+            throw new IllegalArgumentException("Debe especificar la deuda asociada al abono!");
+        }
+        return debt;
+    }
+}
