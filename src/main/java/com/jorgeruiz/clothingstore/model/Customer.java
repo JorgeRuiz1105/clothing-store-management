8c801d96090cb6e829135cb0fe3d32c04537f2d6
Jorge Ruiz <jorge_ruiz82212@elpoli.edu.co>
2026-09-16 18:40:37 -0500

    feat(customer): Add Customer entity

---
+++ src/main/java/com/jorgeruiz/clothingstore/model/Customer.java
@@ -0,0 +1,67 @@
+package com.jorgeruiz.clothingstore.model;
+
+public class Customer {
+    private int id;
+    private String document;
+    private String name;
+    private String phone;
+    private String address;
+
+    public Customer(int id, String document, String name, String phone, String address) {
+        this.id = id;
+        this.document = validateDocument(document);
+        this.name = name;
+        this.phone = validatePhone(phone);
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = validateDocument(document);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = validatePhone(phone);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String validateDocument(String document){
+        if(!document.matches("^[0-9]{1,10}$")){
+            throw new IllegalArgumentException("Formato de cedula invalido!");
+        }
+        return document;
+    }
+
+    private String validatePhone(String phone){
+        if(!phone.matches("^[0-9]{1,10}$")){
+            throw new IllegalArgumentException("Formato de telefono invalido!");
+        }
+        return phone;
+    }
+}
